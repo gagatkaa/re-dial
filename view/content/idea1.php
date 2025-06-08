c13ecbd483338392e41ddbc1326fea4a22de9fac
gagatkaa <atomaszewska361@gmail.com>
2025-06-08 17:06:01 +0200

    feat:idea1 page

--- view/content/idea1.php
+++ view/content/idea1.php
@@ -1,10 +1,25 @@
 <section class="idea__content container">
-    <div class="idea">
+    <article class="idea">
         <header class="idea__header">
-            <h2 class="idea__title">Silent Hotline</h2>
+            <h2>Silent Hotline</h2>
+            <p class="idea__subtitle">Talk it out, even if no one’s listening.</p>
         </header>
-        <p class="idea__subtitle">Talk it out, even if no one’s listening.</p>
-    </div>
+        <div class="idea__description">
+            <p class="idea__instructions">
+                <span class="idea__howto-label">How to make it:</span><br>
+                Keep the receiver from an old phone. That’s it. Or just simulate the act with any object that fits in your hand.
+
+               
+
+            </p>
+            <p class="idea__instructions">
+                <span class="idea__howto-label">How it helps:</span><br>
+                Talking out loud can be a powerful way to process emotions, especially during panic or stress. It creates a safe, judgment-free moment of expression, like calling a version of yourself who always listens.
+
+            
+        </p>
+        </div>
+    </article>
     <div class="navigation__button">
         <a href="index.php?page=idea2" class="arrow-button-svg">
             <svg xmlns="http://www.w3.org/2000/svg" width="230" height="47" viewBox="0 0 230 47" fill="none">
---
+++ view/content/idea2.php
@@ -0,0 +1,36 @@
+<section class="idea__content container">
+    <article class="idea">
+        <header class="idea__header">
+            <h2>Silent Hotline</h2>
+            <p class="idea__subtitle">Talk it out, even if no one’s listening.</p>
+        </header>
+        <div class="idea__description">
+            <p class="idea__instructions">
+                <span class="idea__howto-label">How to make it:</span><br>
+                Keep the receiver from an old phone. That’s it. Or just simulate the act with any object that fits in
+                your hand.
+
+
+
+            </p>
+            <p class="idea__instructions">
+                <span class="idea__howto-label">How it helps:</span><br>
+                Talking out loud can be a powerful way to process emotions, especially during panic or stress. It
+                creates a safe, judgment-free moment of expression, like calling a version of yourself who always
+                listens.
+
+
+            </p>
+        </div>
+    </article>
+    <div class="navigation__button">
+        <a href="index.php?page=idea2" class="arrow-button-svg">
+            <svg xmlns="http://www.w3.org/2000/svg" width="230" height="47" viewBox="0 0 230 47" fill="none">
+                <path
+                    d="M0 5C0 2.23858 2.23858 0 5 0H191.756C193.556 0 195.323 0.485852 196.87 1.40629L226.779 19.2032C230.036 21.1416 230.036 25.8584 226.779 27.7969L196.87 45.5937C195.323 46.5141 193.556 47 191.756 47H5C2.23858 47 0 44.7614 0 42V5Z"
+                    fill="#BFA34E" />
+            </svg>
+            <span class="arrow-label">Breath Focus Spinner</span>
+        </a>
+    </div>
+</section>
